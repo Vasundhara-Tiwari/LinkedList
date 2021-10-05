8fc21efe58323d115b4a834ade356b628df7d107
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-10-05 10:47:35 +0530

    Initial Commit

--- src/linkedLists/SinglyLinkedList.java
+++ src/linkedLists/SinglyLinkedList.java
@@ -2,12 +2,25 @@ package linkedLists;
 
 public class SinglyLinkedList {
     public ListNode head;
-    private static class ListNode{
+    private static class ListNode {
         private int data;
         private ListNode next;
-        public ListNode(int data){
+
+        public ListNode(int data) {
             this.data = data;
             this.next = null;
         }
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+
+        sll.head.next = second;
+        second.next = third;
+        third.next = fourth;
     }
 }
