8288d7c74fc259e9a5d9d558860c1e4d11171b60
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-18 15:15:33 +0530

    Creating a class ListNode in DoublyLinkedList

--- src/linkedLists/DoublyLinkedList.java
+++ src/linkedLists/DoublyLinkedList.java
@@ -1,5 +1,15 @@
 package linkedLists;
 
 public class DoublyLinkedList {
+    private class ListNode
+    {
+        private int data;
+        private ListNode next;
+        private ListNode previous;
 
+        public ListNode(int data)
+        {
+            this.data=data;
+        }
+    }
 }
